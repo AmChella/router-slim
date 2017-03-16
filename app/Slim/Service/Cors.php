a9ba4a4f72c281bf78762c4d117cea2974534d87
Chellapandi S <chellapandi.s@tnqsoftware.co.in>
2017-03-16 18:00:20 +0530

    Base Version

---
+++ app/Slim/Service/Cors.php
@@ -0,0 +1,133 @@
+<?php
+namespace CorsSlim;
+
+// https://developer.mozilla.org/en-US/docs/Web/HTTP/Access_control_CORS
+class Cors {
+    protected $settings;
+
+    public function __construct($settings = array()) {
+        $this->settings = array_merge(array(
+                'origin' => '*',    // Wide Open!
+                'allowMethods' => 'GET,HEAD,PUT,POST,DELETE'
+                ), $settings);
+    }
+
+    protected function setOrigin($req, $rsp) {
+        $origin = $this->settings['origin'];
+        if (is_callable($origin)) {
+            // Call origin callback with request origin
+            $origin = call_user_func($origin,
+                                    $req->getHeader("Origin")
+                                    );
+        }
+
+        // handle multiple allowed origins
+        if(is_array($origin)) {
+
+            $allowedOrigins = $origin;
+
+            $origin = null;
+
+            // but use a specific origin if there is a match
+            foreach($allowedOrigins as $allowedOrigin) {
+                foreach($req->getHeader("Origin") as $reqOrig) {
+                    if($allowedOrigin === $reqOrig) {
+                        $origin = $allowedOrigin;
+                        break;
+                    }
+                }
+                if (!is_null($origin)) {
+                    break;
+                }
+            }
+
+            if (is_null($origin)) {
+                // default to the first allowed origin
+                $origin = reset($allowedOrigins);                
+            }
+        }
+
+        return $rsp->withHeader('Access-Control-Allow-Origin', $origin);
+    }
+
+    protected function setExposeHeaders($req, $rsp) {
+        if (isset($this->settings['exposeHeaders'])) {
+            $rsp = $rsp->withAddedHeader('Access-Control-Expose-Headers', $this->settings['exposeHeaders']);
+        }
+
+        return $rsp;
+    }
+    
+    protected function setMaxAge($req, $rsp) {
+        if (isset($this->settings['maxAge'])) {
+            $rsp = $rsp->withHeader('Access-Control-Max-Age', $this->settings['maxAge']);
+        }
+
+        return $rsp;
+    }
+
+    protected function setAllowCredentials($req, $rsp) {
+        if (isset($this->settings['allowCredentials']) && $this->settings['allowCredentials'] === True) {
+            $rsp = $rsp->withHeader('Access-Control-Allow-Credentials', 'true');
+        }
+
+        return $rsp;
+    }
+
+    protected function setAllowMethods($req, $rsp) {
+        if (isset($this->settings['allowMethods'])) {
+            $rsp = $rsp->withHeader('Access-Control-Allow-Methods', $this->settings['allowMethods']);
+        }
+
+        return $rsp;
+    }
+
+    protected function setAllowHeaders($req, $rsp) {
+        if (isset($this->settings['allowHeaders'])) {
+            $allowHeaders = $this->settings['allowHeaders'];
+        }
+        else {  // Otherwise, use request headers
+            $allowHeaders = $req->getHeader("Access-Control-Request-Headers");
+        }
+
+        if (isset($allowHeaders)) {
+            $rsp = $rsp->withHeader('Access-Control-Allow-Headers', $allowHeaders);
+        }
+
+        return $rsp;
+    }
+
+    protected function setCorsHeaders($req, $rsp) {
+        // http://www.html5rocks.com/static/images/cors_server_flowchart.png
+        // Pre-flight
+        if ($req->isOptions()) {
+            $rsp = $this->setOrigin($req, $rsp);
+            $rsp = $this->setMaxAge($req, $rsp);
+            $rsp = $this->setAllowCredentials($req, $rsp);
+            $rsp = $this->setAllowMethods($req, $rsp);
+            $rsp = $this->setAllowHeaders($req, $rsp);
+        }
+        else {
+            $rsp = $this->setOrigin($req, $rsp);
+            $rsp = $this->setExposeHeaders($req, $rsp);
+            $rsp = $this->setAllowCredentials($req, $rsp);
+        }
+
+        return $rsp;
+    }
+
+    public function __invoke($request, $response, $next) {
+        $response = $this->setCorsHeaders($request, $response);
+        if(!$request->isOptions()) {
+            $response = $next($request, $response);
+        }
+
+        return $response;
+    }
+
+    public static function routeMiddleware($settings = array()) {
+        $cors = new CorsSlim($settings);
+        return $cors;
+    }
+}
+?>
---
+++ app/Slim/Util/Container.php
@@ -0,0 +1,85 @@
+<?php
+namespace Tnq\Slim\Util;
+
+use \Exception;
+use Pimple\Container as Pimple;
+use app\Slim\Service\Router;
+
+class Container {
+    private $application;
+
+    public function __construct(Array $routes, Pimple $instances, $cors = []) {
+        $this->application = [];
+        if (empty($routes) === true) {
+            throw new Exception('routes.value.is.empty');
+        }
+
+        array_push($this->application, [
+            'routes' => $routes, 'cors' => $cors, 'object' => $instances
+            ]);
+        $this->initialize($this->application);
+    }
+
+    private function initialize($a) {
+
+        $a['router'] = function($c) use($a) {
+            return new router($a);
+        };
+
+        $a['requestHandler'] = function($c) use($a) {
+            return new RequestHandler($a);
+        };
+
+        $a['cors'] = function ($c) use ($a) {
+            $corsValues = $a['corsHandler'];
+            $allowedOrigin = $this->getAllowedOrigin(
+                $corsValues['origin']
+            );
+
+            $corsOptions = [
+                'origin' => $allowedOrigin,
+                'allowHeaders' => $corsValues['accept_headers'],
+                'allowCredentials',  $corsValues['allow_credentials']
+            ];
+
+            return $corsOptions;
+        };
+
+        $a['corsHandler'] = function () use ($a) {
+            $hasCrossOrigin = array_key_exists('cors', $a['cors']);
+            if (empty($hasCrossOrigin) === true) {
+                return [
+                    'origin' => '*',
+                    'allowCredentials' => 'false',
+                    'aollowHeaders' => 'Content-Type, X-Requested-With',
+                ];
+            }
+
+            return $a['cors'];
+        };
+
+        return $a;
+    }
+
+    private function getOriginHeader() {
+        if (isset($_SERVER['HTTP_ORIGIN'])) {
+            return $_SERVER['HTTP_ORIGIN'];
+        }
+        return null;
+    }
+
+    private function getAllowedOrigin($allowedOriginList) {
+        $originHeader = $this->getOriginHeader();
+        if (empty($originHeader) ) {
+            return null;
+        }
+
+        $matchedKey = array_search($originHeader, $allowedOriginList);
+        if ($matchedKey >= 0) {
+            $matchedOrigin = $allowedOriginList[$matchedKey];
+            return $matchedOrigin;
+        }
+
+        return null;
+    }
+}
